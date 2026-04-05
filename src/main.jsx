3a8896327c8696cef0413ccc8c15ab381a38c5f2
Leo wilis <leowilis9898@gmail.com>
2026-04-05 13:17:56 +0700

    feat: update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Router from './router/Router'
 import './index.css'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
