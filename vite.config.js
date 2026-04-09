3c01b2f66a9e935e83ebe14caf17511ef568b0f7
Leo wilis <leowilis9898@gmail.com>
2026-04-09 17:40:53 +0700

    chore: add tailwindcss in plugins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
