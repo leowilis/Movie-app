cf35c9424918661087a9549416537fe8eb9620a8
Leo wilis <leowilis9898@gmail.com>
2026-04-11 14:17:03 +0700

    feat: add HomeContent component

---
+++ src/features/home/components/HomeContent.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "./HeroSection";
+
+export default function HomeContent() {
+  return (
+    <section className="relative w-full overflow-hidden pt-20">
+      <HeroSection />
+      
+    </section>
+  )
+}
