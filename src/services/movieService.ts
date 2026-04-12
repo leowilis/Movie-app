b339c450e62a69fe244c797d064c0c801052f104
Leo wilis <leowilis9898@gmail.com>
2026-04-12 15:48:05 +0700

    chore: update movie videos

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,5 @@
 import { api } from '@/lib/api';
-import { Movie, MovieResponse } from '@/features/types/Movie';
+import { Movie, MovieResponse, Video } from '@/features/types/Movie';
 
 export const getNowPlaying = async (): Promise<Movie[]> => {
   const { data } = await api.get<MovieResponse>('/movie/now_playing');
@@ -14,4 +14,11 @@ export const getTranding = async (): Promise<Movie[]> => {
 export const getPopular = async (): Promise<Movie[]> => {
     const { data } = await api.get<MovieResponse>('/movie/popular');
     return data.results;
+}
+
+export const getMovieVideos = async (id: string): Promise<Video[]> => {
+  const { data } = await api.get<{ results: Video[] }>(`/movie/${id}/videos`)
+  return data.results.filter(
+    (v) => v.site === 'YouTube' && v.type === 'Trailer'
+  )
 }
