c96565b7a72a61cea5b5d0dbe589cb55c0feb39d
Leo wilis <leowilis9898@gmail.com>
2026-04-13 17:04:48 +0700

    feat: update nav menu items

---
+++ src/constants/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { label: 'Home', href: '/' },
+  { label: 'Search', href: '/search' },
+  { label: 'Favorites', href: '/favorites' },
+  { label: 'Now Playing', href: '/now-playing' },
+];
