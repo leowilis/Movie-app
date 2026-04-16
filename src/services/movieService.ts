8f59b24d86ec051a059b9679b32f5479957fc6cf
Leo wilis <leowilis9898@gmail.com>
2026-04-16 16:39:35 +0700

    feat: add getnewRelease

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -6,19 +6,24 @@ export const getNowPlaying = async (): Promise<Movie[]> => {
   return data.results;
 };
 
-export const getTranding = async (): Promise<Movie[]> => {
-    const { data } = await api.get<MovieResponse>('/trending/movie/day');
-    return data.results;
+export const getTrending = async (): Promise<Movie[]> => {
+  const { data } = await api.get<MovieResponse>('/trending/movie/day');
+  return data.results;
 };
 
 export const getPopular = async (): Promise<Movie[]> => {
-    const { data } = await api.get<MovieResponse>('/movie/popular');
-    return data.results;
-}
+  const { data } = await api.get<MovieResponse>('/movie/popular');
+  return data.results;
+};
 
 export const getMovieVideos = async (id: string): Promise<Video[]> => {
-  const { data } = await api.get<{ results: Video[] }>(`/movie/${id}/videos`)
+  const { data } = await api.get<{ results: Video[] }>(`/movie/${id}/videos`);
   return data.results.filter(
-    (v) => v.site === 'YouTube' && v.type === 'Trailer'
-  )
-}
+    (v) => v.site === 'YouTube' && v.type === 'Trailer',
+  );
+};
+
+export const getNewRelease = async (): Promise<Movie[]> => {
+  const { data } = await api.get<MovieResponse>('/movie/now_playing');
+  return data.results;
+};
