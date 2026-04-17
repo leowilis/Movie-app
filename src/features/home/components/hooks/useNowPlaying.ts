da7832e3bfb71923c799a8f6e4b74d668f4b253b
Leo wilis <leowilis9898@gmail.com>
2026-04-17 21:15:03 +0700

    refactor: remove unused useNowPlaying hook

--- src/features/home/components/hooks/useNowPlaying.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getNowPlaying } from "@/services/movieService";
-import { Movie } from "@/features/types/Movie";
-
-export const useNowPlaying = () =>
-  useQuery<Movie[]>({
-    queryKey: ["now-playing"],
-    queryFn: getNowPlaying,
-  });
