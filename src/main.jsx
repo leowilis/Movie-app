d574b53b630329b9aa37f9d07b8baca5108a97f2
Leo wilis <leowilis9898@gmail.com>
2026-05-05 17:10:57 +0700

    fix: correct router import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import Router from './router/Router'
+import Router from './router'
 import './index.css'
 
 const queryClient = new QueryClient();
